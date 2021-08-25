4ed4274434baf10af8bcf9b96f87fa39dd55e7e1
Damiao <tower_gamer@hotmail.com>
2021-08-25 13:13:31 -0300

    Expenses model

---
+++ expenses/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from authentication.models import User 
+from helpers.models import TrackingModel
+
+
+class Expenses(TrackingModel):
+
+    CATEGORY_OPTIONS = [
+        ('ONLINE_SERVICES', 'ONLINE_SERVICES'),
+        ('TRAVEL', 'TRAVEL'),
+        ('FOOD', 'FOOD'),
+        ('RENT', 'RENT'),
+        ('OTHERS', 'OTHERS'),
+    ]
+
+    category = models.CharField(choices=CATEGORY_OPTIONS, max_length=15)
+    amount = models.DecimalField(max_length=15, max_digits=10, decimal_places=2)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
