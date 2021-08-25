e3d3fc2adebcff2dd299c977fb2aa29fe1cfbce9
Damiao <tower_gamer@hotmail.com>
2021-08-24 21:37:51 -0300

    Django admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff')
