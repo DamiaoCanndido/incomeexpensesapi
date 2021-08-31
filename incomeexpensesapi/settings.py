032710fd313f645c5681fe934cddb9c7f93feffb
Damiao <tower_gamer@hotmail.com>
2021-08-31 18:56:11 -0300

    Userstats 2

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'authentication',
     'expenses',
     'incomes',
+    'userstats',
 ]
 
 SWAGGER_SETTINGS = {
--- userstats/views.py
+++ userstats/views.py
@@ -1,10 +1,34 @@
+from typing import cast
 from rest_framework.views import APIView
 import datetime
 from expenses.models import Expense
+from rest_framework import status, response
 
 
 class ExpenseSummaryStats(APIView):
+
+    def get_categories(self, expense):
+        return expense.category
+
+    def get_amount_for_category(self, expense_list, category):
+        expenses = expense_list.filter(category=category)
+        amount = 0
+        for expense in expenses:
+            amount += expense.amount
+        return {'amount': str(amount)}
+
     def get(self, request):
         todays_date = datetime.date.today()
         ayear_ago = todays_date-datetime.timedelta(days=30*12)
-        expenses = Expense.objects.filter(owner=request.user, date__gte=ayear_ago, date__lte=todays_date)
+        expenses = Expense.objects.filter(owner=request.user)
+
+        final = {}
+        categories = list(set(map(self.get_categories, expenses)))
+
+        for expense in expenses:
+            for category in categories:
+                final[category] = self.get_amount_for_category(expenses, category)
+
+        return response.Response({'category_data': final}, status=status.HTTP_200_OK)
+        
+
