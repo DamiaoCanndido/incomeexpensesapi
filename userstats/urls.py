38fb874acd50d6bed28c9bec475f3505dc89be09
Damiao <tower_gamer@hotmail.com>
2021-08-31 21:45:54 -0300

    Incomes chart

--- userstats/urls.py
+++ userstats/urls.py
@@ -1,7 +1,8 @@
-from .views import ExpenseSummaryStats
+from .views import ExpenseSummaryStats, IncomeSourcesSummaryStats
 from django.urls import path
 
 
 urlpatterns = [
-    path('expense-category-data', ExpenseSummaryStats.as_view(), name='expense-category-data')
+    path('expense-category-data', ExpenseSummaryStats.as_view(), name='expense-category-data'),
+    path('income-category-data', IncomeSourcesSummaryStats.as_view(), name='income-category-data')
 ]
--- userstats/views.py
+++ userstats/views.py
@@ -2,10 +2,14 @@ from typing import cast
 from rest_framework.views import APIView
 import datetime
 from expenses.models import Expense
+from incomes.models import Income
 from rest_framework import status, response
+from rest_framework import permissions
 
 
 class ExpenseSummaryStats(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_categories(self, expense):
         return expense.category
@@ -30,5 +34,35 @@ class ExpenseSummaryStats(APIView):
                 final[category] = self.get_amount_for_category(expenses, category)
 
         return response.Response({'category_data': final}, status=status.HTTP_200_OK)
+
+
+
+class IncomeSourcesSummaryStats(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_sources(self, income):
+        return income.source
+
+    def get_amount_for_sources(self, income_list, source):
+        incomes = income_list.filter(source=source)
+        amount = 0
+        for income in incomes:
+            amount += income.amount
+        return {'amount': str(amount)}
+
+    def get(self, request):
+        todays_date = datetime.date.today()
+        ayear_ago = todays_date-datetime.timedelta(days=30*12)
+        incomes = Income.objects.filter(owner=request.user)
+
+        final = {}
+        sources = list(set(map(self.get_sources, incomes)))
+
+        for income in incomes:
+            for source in sources:
+                final[source] = self.get_amount_for_sources(incomes, source)
+
+        return response.Response({'income_source_data': final}, status=status.HTTP_200_OK)
         
 
