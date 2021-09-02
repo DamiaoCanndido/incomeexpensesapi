eae75368397e1e2df37ade69cd4267d857a96af3
Damiao <tower_gamer@hotmail.com>
2021-09-02 20:30:13 -0300

    Export swagger routes to postman

--- incomeexpensesapi/urls.py
+++ incomeexpensesapi/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('social-auth/', include('social_auth.urls')),
     path('userstats/', include('userstats.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
